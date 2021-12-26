f939a1a3451072ab9ff3ca2e77b56ded55dc1b89
hdowa <hdowa@192.168.1.20>
2021-12-26 09:47:17 +0100

    initial commit

---
+++ src/main/java/info/fivecdesign/gamecollection/portal/AllGamesConsole.java
@@ -0,0 +1,10 @@
+package info.fivecdesign.gamecollection.portal;
+
+public class AllGamesConsole implements Portal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+	}
+
+}
