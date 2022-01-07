a4f3fd96edf90e6a556935cd14a53c5a50d385e6
hdowa <hdowa@192.168.1.20>
2022-01-07 12:12:06 +0100

    hiding even more stuff

--- src/main/java/info/fivecdesign/gamecollection/blackjack/backend/Card.java
+++ src/main/java/info/fivecdesign/gamecollection/blackjack/backend/Card.java
@@ -1,6 +1,6 @@
 package info.fivecdesign.gamecollection.blackjack.backend;
 
-public enum Card {
+enum Card {
 	
     C1(1), C2(2), C3(3), C4(4), C5(5), C6(6), C7(7), C8(8), C9(9), C10(10), 
     JACK(10), QUEEN(10), KING(10), ACE(11,1);
