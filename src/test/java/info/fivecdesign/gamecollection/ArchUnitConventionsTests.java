d8f6cb510df285f6b694b0dac12656f6df350515
hdowa <hdowa@192.168.1.20>
2022-01-07 12:31:18 +0100

    added a custom condition

--- src/test/java/info/fivecdesign/gamecollection/ArchUnitConventionsTests.java
+++ src/test/java/info/fivecdesign/gamecollection/ArchUnitConventionsTests.java
@@ -1,13 +1,18 @@
 package info.fivecdesign.gamecollection;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 
 import info.fivecdesign.gamecollection.earthtrivia.backend.generators.Generator;
 import info.fivecdesign.gamecollection.portal.GameConsole;
-
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "info.fivecdesign.gamecollection")
 public class ArchUnitConventionsTests {
@@ -19,5 +24,24 @@ public class ArchUnitConventionsTests {
 	@ArchTest
 	public final static ArchRule consoleNamePrefix = classes().that().implement(GameConsole.class)
 						.should().haveSimpleNameStartingWith("Console");
+	
+	private static ArchCondition<JavaClass> hasMethodNext =
+		    new ArchCondition<JavaClass>("has a method called next") {
+		        @Override
+		        public void check(JavaClass item, ConditionEvents events) {
+		    		boolean found = false;
+		    		for (JavaMethod method : item.getAllMethods()) {
+		    			if (method.getName().equals("next")) {
+		    				found = true;
+		    			}
+		    		}
+		    		if (!found) {
+		    			events.add(SimpleConditionEvent.violated(item, "Class does not have a next method!"));
+		    		}
+		        }
+		    };
+		    
+	@ArchTest
+	public final static ArchRule selectorsHaveNextMethod = classes().that().haveSimpleNameEndingWith("Selector").should(hasMethodNext);
 
 }
